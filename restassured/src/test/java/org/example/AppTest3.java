8f7fd32603b9d5d64026a364fd2b4f3e07983baf
shibaram <email2srout@gmail.com>
2023-12-05 22:16:53 +0530

    OK

---
+++ restassured/src/test/java/org/example/AppTest3.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.*;
+
+
+public class AppTest3{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+}
